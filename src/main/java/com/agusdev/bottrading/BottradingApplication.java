adcbf27fdb673bea9fac2d2f9b25088b0146fcbb
agusNazer <agustin.nazer@hotmail.com>
2024-12-25 20:18:31 -0300

    binance api conected

--- src/main/java/com/agusdev/bottrading/BottradingApplication.java
+++ src/main/java/com/agusdev/bottrading/BottradingApplication.java
@@ -2,12 +2,24 @@ package com.agusdev.bottrading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BottradingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BottradingApplication.class, args);
+		// SpringApplication.run(BottradingApplication.class, args); // esta es la linea predeterminada por spring para el arranque
+		 // Ejecuta la aplicación Spring Boot y obtiene el contexto
+		 ConfigurableApplicationContext context = SpringApplication.run(BottradingApplication.class, args);
+
+		// Instancia del servicio con las claves API
+		//  String apiKey = "USBdYlrWJC3bmjI0ELNRagQegByX5Exe7w817ytoaT0poMsAzblaIYl70aK9ry9n";
+		//  String secretKey = "U5hBLlOR7O3dwRv5bHIsjTSldnUyEz1dPWkGgfQbSxc5W4rQIOedgmXOd6pUiiol";
+		 BinanceService binanceService = context.getBean(BinanceService.class);
+ 
+		 // Prueba del método getPrice
+		 String price = binanceService.getPrice("BTCUSDT");
+		 System.out.println("Precio de BTC/USDT: " + price);
 	}
 
 }
